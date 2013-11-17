f166bc36e1a3016e41079af68c305c9596a2ba33
Gergo Debreczeni <gergo.debreczeni@yahoo.com>
2013-11-17 05:21:09 +0200

    added notifications

--- js/dataSource.js
+++ js/dataSource.js
@@ -75,6 +75,7 @@
 
     var _getShows = function () {
         return _shows;
+        
     }
 
     var _getShowByName = function (name) {
@@ -140,9 +141,24 @@
             for (var i in subscribers) {
                 subscribers[i].push(new_show);
             }
-            var a = 2;
+
         });
 
+
+        var roamingSettings = Windows.Storage.ApplicationData.current.roamingSettings;
+        var settingName = "exampleCompositeSetting";
+
+        var settingName1 = "one";
+        var settingName2 = "hello";
+
+        function compositeSettingsWriteCompositeSetting() {
+            var composite = new Windows.Storage.ApplicationDataCompositeValue();
+            composite[settingName1] = 1; // example value
+            composite[settingName2] = "world"; // example value
+            roamingSettings.values[settingName] = composite;
+
+            compositeSettingsDisplayOutput();
+        }
     }
 
     var _addShowToFavourites = function (name) {
@@ -151,13 +167,74 @@
         for (i in shows) {
             if (shows[i].title == name) {
                 temp_show = new Show(shows[i].title, shows[i].description, shows[i].picture);
+                temp_show.category = "Favorites";
                 _categories["Favorites"].shows.push(temp_show);
                 for (var i in subscribers) {
                     subscribers[i].push(temp_show);
                 }
+                sendTileTextNotification(temp_show.title);
                 return;
             }
         }
+    }
+    function sendTileTextNotification(showName) {
+        // Note: This sample contains an additional project, NotificationsExtensions.
+        // NotificationsExtensions exposes an object model for creating notifications, but you can also modify the xml
+        // of the notification directly. See the additional function sendTileTextNotificationWithXmlManipulation to see how
+        // to do it by modifying Xml directly, or sendTileTextNotificationWithStringManipulation to see how to do it
+        // by modifying strings directly
+
+        // create the wide template
+        var tileContent = NotificationsExtensions.TileContent.TileContentFactory.createTileWideText03();
+        tileContent.textHeadingWrap.text =showName+ " added to favourites."  ;
+
+        // Users can resize tiles to square or wide.
+        // Apps can choose to include only square assets (meaning the app's tile can never be wide), or
+        // include both wide and square assets (the user can resize the tile to square or wide).
+        // Apps cannot include only wide assets.
+
+        // Apps that support being wide should include square tile notifications since users
+        // determine the size of the tile.
+
+        // create the square template and attach it to the wide template
+        var squareTileContent = NotificationsExtensions.TileContent.TileContentFactory.createTileSquareText04();
+        squareTileContent.textBodyWrap.text = showName + "was added to favourites.";
+        tileContent.squareContent = squareTileContent;
+
+        // send the notification
+        Windows.UI.Notifications.TileUpdateManager.createTileUpdaterForApplication().update(tileContent.createNotification());
+
+        WinJS.log && WinJS.log(tileContent.getContent(), "sample", "status");
+    }
+
+    function sendTileLocalImageNotificationWithXmlManipulation() {
+        // get a XML DOM version of a specific template by using getTemplateContent
+        var tileXml = Windows.UI.Notifications.TileUpdateManager.getTemplateContent(Windows.UI.Notifications.TileTemplateType.tileWideImageAndText01);
+
+        // get the text attributes for this template and fill them in
+        var tileTextAttributes = tileXml.getElementsByTagName("text");
+        tileTextAttributes[0].appendChild(tileXml.createTextNode("This tile notification uses ms-appx images"));
+
+        // get the image attributes for this template and fill them in
+        var tileImageAttributes = tileXml.getElementsByTagName("image");
+        tileImageAttributes[0].setAttribute("src", "ms-appx:///images/redWide.png");
+
+        // fill in a version of the square template returned by GetTemplateContent
+        var squareTileXml = Windows.UI.Notifications.TileUpdateManager.getTemplateContent(Windows.UI.Notifications.TileTemplateType.tileSquareImage);
+        var squareTileImageAttributes = squareTileXml.getElementsByTagName("image");
+        squareTileImageAttributes[0].setAttribute("src", "ms-appx:///images/graySquare.png");
+
+        // include the square template into the notification
+        var node = tileXml.importNode(squareTileXml.getElementsByTagName("binding").item(0), true);
+        tileXml.getElementsByTagName("visual").item(0).appendChild(node);
+
+        // create the notification from the XML
+        var tileNotification = new Windows.UI.Notifications.TileNotification(tileXml);
+
+        // send the notification to the app's application tile
+        Windows.UI.Notifications.TileUpdateManager.createTileUpdaterForApplication().update(tileNotification);
+
+        WinJS.log && WinJS.log(tileXml.getXml(), "sample", "status");
     }
 
     WinJS.Namespace.define("App.DataSource", {
--- js/default.js
+++ js/default.js
@@ -95,7 +95,7 @@
     };
 
     app.start();
-
+    
 
     //  doClickAdd();
 
