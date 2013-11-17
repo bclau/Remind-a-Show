d6478cc64efba17d3e0d202e47c80ddc38fe0f6a
Gergo Debreczeni <gergo.debreczeni@yahoo.com>
2013-11-17 06:18:06 +0200

    fixed calendar

--- js/util/calendarUtils.js
+++ js/util/calendarUtils.js
@@ -1,13 +1,14 @@
 ﻿(function () {
     "use strict";
 
-    WL.init();
+    WL.init({
+        scope: "wl.events_create"}
+        );
+
 
     var _addEvent = function(name, description, start_time, end_time) {
-        WL.login({
-            scope: "wl.events_create"
-        }).then(
-                  function (response) {
+       
+                   
                       WL.api({
                           path: "me/events",
                           method: "POST",
@@ -15,7 +16,7 @@
                               name: name,
                               description: description,
                               start_time: start_time, //"2013-11-16T01:30:00-08:00",
-                              end_time: end_time, //"2013-11-16T03:00:00-08:00",
+                              end_time: new Date(Date.parse(start_time)+60), //"2013-11-16T03:00:00-08:00",
                               //location: "Coho Vineyard and Winery, 123 Main St., Redmond WA 19532",
                               is_all_day_event: "false",
                               availability: "busy",
@@ -23,21 +24,13 @@
                           }
                       }).then(
                           function (response) {
-                              document.getElementById("resultDiv").innerHTML =
-                                  "ID: " + response.id +
-                                  "<br/>Name: " + response.name;
+                            
                           },
                           function (responseFailed) {
-                              document.getElementById("infoArea").innerText =
-                                  "Error calling API: " + responseFailed.error.message;
+                             
                           }
                       );
-                  },
-                  function (responseFailed) {
-                      document.getElementById("infoArea").innerText =
-                          "Error signing in: " + responseFailed.error_description;
-                  }
-              );
+
 
     }
 
